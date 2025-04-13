92337032d038d1d6296825b3e383e262c71b5804
mpaterakis <emman.paterakis@gmail.com>
2025-04-13 15:33:58 +0300

    Fixed linux build.

--- gui-helper.h
+++ gui-helper.h
@@ -3,6 +3,10 @@
 
 int ShowMessageBox(const char* title, const char* message);
 int SelectionDialog(const char* title, int count, const char** list, int selection);
+
+#if defined(_WIN32) || defined(WIN32) 
+// This function is only used in Windows. It shouldn't exist, but we have to deal with Microsoft.
 wchar_t * ConvertStringToUnicode(char * string);
+#endif
 
 #endif
