0a41f8ee2462b5d4c8eb078b3132ddaa10e2c681
Jozen Blue Martinez <me@jozen.blue>
2026-07-22 13:45:10 +0800

    use getline within readLine and add different win shim

--- string-helpers.c
+++ string-helpers.c
@@ -5,60 +5,95 @@
 
 #if defined(_WIN32) || defined(WIN32)
 #define strcasecmp _stricmp
+
+/*
+getline() shim https://stackoverflow.com/a/47229318
+
+The original code is public domain -- Will Hartung 4/9/2009
+Modifications, public domain as well, by Antti Haapala, 11/10/2017
+   - Switched to getc on 5/23/2019
+   - Proper error handling on IO error and wraparound 
+     check on buffer extension 3/31/2025 - 4/2/2025
+*/
+#include <errno.h>
+#include <stdint.h>
+#define MINIMUM_BUFFER_SIZE 128
+
+// if typedef doesn't exist (msvc, blah)
+typedef intptr_t ssize_t;
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    size_t pos;
+    int c;
+
+    if (lineptr == NULL || stream == NULL || n == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    c = getc(stream);
+    if (c == EOF) {
+        return -1;
+    }
+
+    if (*lineptr == NULL) {
+        *lineptr = malloc(MINIMUM_BUFFER_SIZE);
+        if (*lineptr == NULL) {
+            return -1;
+        }
+        *n = MINIMUM_BUFFER_SIZE;
+    }
+
+    pos = 0;
+    while(c != EOF) {
+        if (pos + 1 >= *n) {
+            size_t new_size = *n + (*n >> 2);
+
+            // have some reasonable minimum
+            if (new_size < MINIMUM_BUFFER_SIZE) {
+                new_size = MINIMUM_BUFFER_SIZE;
+            }
+            
+            // size_t wraparound
+            if (new_size <= *n) {
+                errno = ENOMEM;
+                return -1;
+            }
+
+            // Note you might also want to check that PTRDIFF_MAX
+            // is not exceeded!
+
+            char *new_ptr = realloc(*lineptr, new_size);
+            if (new_ptr == NULL) {
+                return -1;
+            }
+            *n = new_size;
+            *lineptr = new_ptr;
+        }
+
+        ((unsigned char *)(*lineptr))[pos ++] = c;
+        if (c == '\n') {
+            break;
+        }
+        c = getc(stream);
+    }
+
+    (*lineptr)[pos] = '\0';
+    
+    // if an IO error occurred, return -1
+    if (c == EOF && !feof(stream)) {
+        return -1;
+    }
+
+    // otherwise we successfully read until the end-of-file
+    // or the delimiter
+    return pos;
+}
 #endif
 
-// Overriding getline_custom
-// https://stackoverflow.com/a/3501681/16642426
 size_t readLine(char** lineptr, size_t* n, FILE* stream) {
-	char* bufptr = NULL;
-	char* p = bufptr;
-	size_t size;
-	int c;
-
-	if (lineptr == NULL) {
-		return -1;
-	}
-	if (stream == NULL) {
-		return -1;
-	}
-	if (n == NULL) {
-		return -1;
-	}
-	bufptr = *lineptr;
-	size = *n;
-
-	c = fgetc(stream);
-	if (c == EOF) {
-		return -1;
-	}
-	if (bufptr == NULL) {
-		bufptr = malloc(128);
-		if (bufptr == NULL) {
-			return -1;
-		}
-		size = 128;
-	}
-	p = bufptr;
-	while (c != EOF) {
-		if ((p - bufptr) > (size - 1)) {
-			size = size + 128;
-			bufptr = realloc(bufptr, size);
-			if (bufptr == NULL) {
-				return -1;
-			}
-		}
-		*p++ = c;
-		if (c == '\n') {
-			break;
-		}
-		c = fgetc(stream);
-	}
-
-	*p++ = '\0';
-	*lineptr = bufptr;
-	*n = size;
-
-	return p - bufptr - 1;
+    ssize_t result = getline(lineptr, n, stream);
+    return (result < 0) ? (size_t)-1 : (size_t)result;
 }
 
 // Check if a string starts with a given substring
--- string-helpers.h
+++ string-helpers.h
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-// Overriding getline_custom
-// https://stackoverflow.com/a/3501681/16642426
 size_t readLine(char**, size_t*, FILE*);
 
 // Check if a string starts with a given substring
